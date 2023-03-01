36c825109027c25d74fb99bec262831a59ec9f74
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-01 16:35:51 +0100

    Add LAPKT DFS+ image and script.

--- planners/build-all-images.sh
+++ planners/build-all-images.sh
@@ -10,7 +10,7 @@ OUTDIR=../images/
 ./build-image.sh ../planners/ipc2014-opt-symba/Singularity-01 ${OUTDIR}/ipc2014-opt-symba1.img
 ./build-image.sh ../planners/ipc2018-opt-complementary1/Singularity ${OUTDIR}/ipc2018-opt-complementary1.img
 ./build-image.sh ../planners/ipc2018-opt-complementary2/Singularity ${OUTDIR}/ipc2018-opt-complementary2.img
-./build-image.sh ../planners/ipc2018-opt-decstar/Singularity ${OUTDIR}/ipc2018-opt-decstar.img
+#./build-image.sh ../planners/ipc2018-opt-decstar/Singularity ${OUTDIR}/ipc2018-opt-decstar.img  # unrecognized arguments: --preprocess-options --h2-time-limit
 ./build-image.sh ../planners/ipc2018-opt-delfi/Apptainer ${OUTDIR}/ipc2018-opt-delfi.img
 ./build-image.sh ../planners/ipc2018-opt-fdms/Singularity ${OUTDIR}/ipc2018-opt-fdms.img
 ./build-image.sh ../planners/ipc2018-opt-metis/Singularity ${OUTDIR}/ipc2018-opt-metis.img
@@ -19,7 +19,6 @@ OUTDIR=../images/
 ./build-image.sh ../planners/ipc2018-opt-scorpion/Singularity ${OUTDIR}/ipc2018-opt-scorpion.img
 ./build-image.sh ../planners/ipc2018-opt-symple1/Singularity ${OUTDIR}/ipc2018-opt-symple1.img
 ./build-image.sh ../planners/ipc2018-opt-symple2/Singularity ${OUTDIR}/ipc2018-opt-symple2.img
-
 
 # AGL
 ./build-image.sh ../planners/ipc2014-agl-jasper/Singularity ${OUTDIR}/ipc2014-agl-jasper.img
@@ -30,13 +29,10 @@ OUTDIR=../images/
 ./build-image.sh ../planners/ipc2018-agl-fd-remix/Singularity ${OUTDIR}/ipc2018-agl-fd-remix.img
 ./build-image.sh ../planners/ipc2018-agl-fdss-2018/Singularity ${OUTDIR}/ipc2018-agl-fdss-2018.img
 ./build-image.sh ../planners/ipc2018-agl-lapkt-dual-bfws/Apptainer ${OUTDIR}/ipc2018-agl-lapkt-bfws.img
+./build-image.sh ../planners/ipc2018-agl-lapkt-dfs-plus/Singularity ${OUTDIR}/ipc2018-agl-lapkt-dfs-plus.img
 ./build-image.sh ../planners/ipc2018-agl-mercury2014/Singularity ${OUTDIR}/ipc2018-agl-mercury2014.img
 ./build-image.sh ../planners/ipc2018-agl-merwin/Singularity.agl ${OUTDIR}/ipc2018-agl-merwin.img
 ./build-image.sh ../planners/ipc2018-agl-olcff/Singularity ${OUTDIR}/ipc2018-agl-olcff.img
 ./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity ${OUTDIR}/ipc2018-agl-saarplan.img
-
-
-# SAT
-# ./build-image.sh ../planners/ipc2018-agl-merwin/Singularity.sat ${OUTDIR}/ipc2018-sat-merwin.img  # TODO: no plan found
 
 echo "Finished building images"
