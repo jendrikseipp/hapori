2b9f8069868415cd18abb7b08a9244e4e5b6a2b6
Silvan Sievers <silvan.sievers@unibas.ch>
2023-03-02 13:47:28 +0100

    comment out scorpion no-div and fdms planners

--- planners/build-all-images.sh
+++ planners/build-all-images.sh
@@ -12,10 +12,8 @@ OUTDIR=../images/
 ./build-image.sh ../planners/ipc2018-opt-complementary2/Singularity ${OUTDIR}/ipc2018-opt-complementary2.img
 ./build-image.sh ../planners/ipc2018-opt-decstar/Singularity ${OUTDIR}/ipc2018-opt-decstar.img
 ./build-image.sh ../planners/ipc2018-opt-delfi/Apptainer ${OUTDIR}/ipc2018-opt-delfi.img
-./build-image.sh ../planners/ipc2018-opt-fdms/Singularity ${OUTDIR}/ipc2018-opt-fdms.img
 ./build-image.sh ../planners/ipc2018-opt-metis/Singularity ${OUTDIR}/ipc2018-opt-metis.img
 ./build-image.sh ../planners/ipc2018-opt-planning-pdbs/Singularity ${OUTDIR}/ipc2018-opt-planning-pdbs.img
-./build-image.sh ../planners/ipc2018-opt-scorpion-nodiv/Singularity ${OUTDIR}/ipc2018-opt-scorpion-nodiv.img
 ./build-image.sh ../planners/ipc2018-opt-scorpion/Singularity ${OUTDIR}/ipc2018-opt-scorpion.img
 ./build-image.sh ../planners/ipc2018-opt-symple1/Singularity ${OUTDIR}/ipc2018-opt-symple1.img
 ./build-image.sh ../planners/ipc2018-opt-symple2/Singularity ${OUTDIR}/ipc2018-opt-symple2.img
