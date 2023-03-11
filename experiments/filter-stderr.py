13984efdd6690e831500043c7e51d0561c9a4cea
Patrick Ferber <patrick.ferber@unibas.ch>
2023-03-11 12:06:03 +0100

    update auxiliary scripts for experiments

--- experiments/filter-stderr.py
+++ experiments/filter-stderr.py
@@ -15,6 +15,7 @@ IGNORE_PATTERNS = [
     "Killed",
     "underlay of /etc/localtime required more than",
     "sched_setaffinity failed: : Invalid argument",
+    "warning: error reading memory from procfile",
     "ls: cannot access", # Somewhere symple1 calls an ls, symple2 doesn;t
 ]
 
