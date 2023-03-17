7bf3bc658e1e4db25f8f13b4b075d11e91e0aee6
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-17 22:07:23 +0100

    Compute optimal stone soup portfolio.

--- learners/stonesoup/learn-portfolios.sh
+++ learners/stonesoup/learn-portfolios.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 OPT_DATA=../../experiments/data/01-opt-planners-eval/properties-hardest.json.xz
 
 #./batch-stonesoup.sh ${OPT_DATA} opt 1800 | tee batch-stonesoup-opt.txt
-#./stonesoup.py --track opt ${OPT_DATA} 110 | tee stonesoup-opt.txt
+./stonesoup.py --track opt ${OPT_DATA} 300 | tee stonesoup-opt.txt
 
-./greedy.py ${OPT_DATA} --track opt | tee greedy-opt.txt
+#./greedy.py ${OPT_DATA} --track opt | tee greedy-opt.txt
--- portfolio_driver/portfolios/hapori-stonesoup-opt.py
+++ portfolio_driver/portfolios/hapori-stonesoup-opt.py
@@ -1,5 +1,16 @@
-# TODO: Fill in real planners and configs.
+"""
+Coverage: 1644
+Granularity: 300s
+Runtime: 1745s
+Score: 1644.0
+"""
 PLANNERS = [
-    (321, "ipc2018-opt-delfi", "h2-simpless-dks-blind"),
-    (542, "ipc2014-opt-symba1", None),
+    # ipc2018-opt-scorpion:default
+    (883, ['ipc2018-opt-scorpion', 'default']),
+    # ipc2014-opt-symba1:default
+    (297, ['ipc2014-opt-symba1', 'default']),
+    # ipc2018-opt-metis:metis2
+    (287, ['ipc2018-opt-metis', 'metis2']),
+    # ipc2018-decstar:opt-config06
+    (278, ['ipc2018-decstar', 'opt-config06']),
 ]
