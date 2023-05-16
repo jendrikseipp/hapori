c059338ecd5392a967fe0e4271947567c6736d8a
Silvan Sievers <silvan.sievers@unibas.ch>
2023-05-16 09:51:25 +0200

    keep all found plans

--- portfolio_driver/portfolio_runner.py
+++ portfolio_driver/portfolio_runner.py
@@ -169,19 +169,9 @@ def run(portfolio, domain_file, problem_file, plan_manager, time, memory):
 
         existing_plan_files = [str(plan) for plan_prefix in planprefixes for plan in get_existing_plans(plan_prefix) ]
         print(f"Portfolio computed the following plans: {existing_plan_files}")
-        best_plan_file = existing_plan_files[-1]
-        best_cost = float('inf')
-        for plan_file in existing_plan_files:
-            for line in reversed(list(open(plan_file))):
-                line = line.lower()
-                if 'cost' in line:
-                    cost = int(line.split(' ')[3])
-                    if cost < best_cost:
-                        best_cost = cost
-                        best_plan_file = plan_file
-
-        print(f"Moving the best found plan {best_plan_file} to {plan_manager.get_plan_prefix()}")
-        shutil.move(best_plan_file, plan_manager.get_plan_prefix())
+        for index, plan_file in enumerate(existing_plan_files):
+            print(f"Moving {plan_file} to {plan_manager.get_plan_prefix()}.{index}")
+            shutil.move(plan_file, f"{plan_manager.get_plan_prefix()}.{index}")
 
 
     return returncodes.generate_portfolio_exitcode(list(exitcodes))
