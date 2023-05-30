cb328e61d46e67e9b957098e7340bc039329b614
Silvan Sievers <silvan.sievers@unibas.ch>
2023-05-30 10:00:15 +0200

    adapt build script

--- planners/build-all-images.sh
+++ planners/build-all-images.sh
@@ -7,32 +7,32 @@ cd $(dirname "$0")
 OUTDIR=../images/
 
 # OPT+SAT+AGL
-./build-image.sh ../planners/ipc2018-decstar/Singularity ${OUTDIR}/ipc2018-decstar.img || true
-./build-image.sh ../planners/ipc2018-symple1/Singularity ${OUTDIR}/ipc2018-symple1.img
-./build-image.sh ../planners/ipc2018-symple2/Singularity ${OUTDIR}/ipc2018-symple2.img
+./build-image.sh ../planners/ipc2018-decstar/Apptainer ${OUTDIR}/ipc2018-decstar.img || true
+./build-image.sh ../planners/ipc2018-symple1/Apptainer ${OUTDIR}/ipc2018-symple1.img
+./build-image.sh ../planners/ipc2018-symple2/Apptainer ${OUTDIR}/ipc2018-symple2.img
 
 # SAT+AGL
-./build-image.sh ../planners/ipc2018-saarplan/Singularity ${OUTDIR}/ipc2018-saarplan.img || true
-./build-image.sh ../planners/ipc2018-agl-cerberus/Singularity ${OUTDIR}/ipc2018-agl-cerberus.img
-./build-image.sh ../planners/ipc2018-agl-lapkt-dfs-plus/Singularity ${OUTDIR}/ipc2018-lapkt-dfs-plus.img
+./build-image.sh ../planners/ipc2018-saarplan/Apptainer ${OUTDIR}/ipc2018-saarplan.img || true
+./build-image.sh ../planners/ipc2018-agl-cerberus/Apptainer ${OUTDIR}/ipc2018-agl-cerberus.img
+./build-image.sh ../planners/ipc2018-agl-lapkt-dfs-plus/Apptainer ${OUTDIR}/ipc2018-lapkt-dfs-plus.img
 ./build-image.sh ../planners/ipc2018-agl-lapkt-dual-bfws/Apptainer ${OUTDIR}/ipc2018-lapkt-bfws.img # covers the three LAPKT BFWS planners
-./build-image.sh ../planners/ipc2018-agl-mercury2014/Singularity ${OUTDIR}/ipc2018-agl-mercury2014.img
-./build-image.sh ../planners/ipc2018-agl-merwin/Singularity ${OUTDIR}/ipc2018-agl-merwin.img
-./build-image.sh ../planners/ipc2018-agl-fdss-2018/Singularity ${OUTDIR}/ipc2018-fd-2018.img # covers both FDSS and FD-Remix
+./build-image.sh ../planners/ipc2018-agl-mercury2014/Apptainer ${OUTDIR}/ipc2018-agl-mercury2014.img
+./build-image.sh ../planners/ipc2018-agl-merwin/Apptainer ${OUTDIR}/ipc2018-agl-merwin.img
+./build-image.sh ../planners/ipc2018-agl-fdss-2018/Apptainer ${OUTDIR}/ipc2018-fd-2018.img # covers both FDSS and FD-Remix
 
 # OPT
-./build-image.sh ../planners/ipc2014-opt-symba/Singularity-01 ${OUTDIR}/ipc2014-opt-symba1.img
-./build-image.sh ../planners/ipc2018-opt-complementary2/Singularity ${OUTDIR}/ipc2018-opt-complementary2.img
+./build-image.sh ../planners/ipc2014-opt-symba/Apptainer-01 ${OUTDIR}/ipc2014-opt-symba1.img
+./build-image.sh ../planners/ipc2018-opt-complementary2/Apptainer ${OUTDIR}/ipc2018-opt-complementary2.img
 ./build-image.sh ../planners/ipc2018-opt-delfi/Apptainer ${OUTDIR}/ipc2018-opt-delfi.img
-./build-image.sh ../planners/ipc2018-opt-metis/Singularity ${OUTDIR}/ipc2018-opt-metis.img
-./build-image.sh ../planners/ipc2018-opt-planning-pdbs/Singularity ${OUTDIR}/ipc2018-opt-planning-pdbs.img
-./build-image.sh ../planners/ipc2018-opt-scorpion/Singularity ${OUTDIR}/ipc2018-opt-scorpion.img
+./build-image.sh ../planners/ipc2018-opt-metis/Apptainer ${OUTDIR}/ipc2018-opt-metis.img
+./build-image.sh ../planners/ipc2018-opt-planning-pdbs/Apptainer ${OUTDIR}/ipc2018-opt-planning-pdbs.img
+./build-image.sh ../planners/ipc2018-opt-scorpion/Apptainer ${OUTDIR}/ipc2018-opt-scorpion.img
 
 # AGL
-./build-image.sh ../planners/ipc2014-agl-jasper/Singularity ${OUTDIR}/ipc2014-agl-jasper.img
-./build-image.sh ../planners/ipc2014-agl-madagascar/Singularity-MpC ${OUTDIR}/ipc2014-agl-mpc.img
-./build-image.sh ../planners/ipc2014-agl-probe/Singularity ${OUTDIR}/ipc2014-agl-probe.img
-./build-image.sh ../planners/ipc2018-agl-olcff/Singularity ${OUTDIR}/ipc2018-agl-olcff.img
-./build-image.sh ../planners/ipc2018-agl-freelunch-madagascar/Singularity ${OUTDIR}/ipc2018-agl-freelunch-madagascar.img
+./build-image.sh ../planners/ipc2014-agl-jasper/Apptainer ${OUTDIR}/ipc2014-agl-jasper.img
+./build-image.sh ../planners/ipc2014-agl-madagascar/Apptainer-MpC ${OUTDIR}/ipc2014-agl-mpc.img
+./build-image.sh ../planners/ipc2014-agl-probe/Apptainer ${OUTDIR}/ipc2014-agl-probe.img
+./build-image.sh ../planners/ipc2018-agl-olcff/Apptainer ${OUTDIR}/ipc2018-agl-olcff.img
+./build-image.sh ../planners/ipc2018-agl-freelunch-madagascar/Apptainer ${OUTDIR}/ipc2018-agl-freelunch-madagascar.img
 
 echo "Finished building images"
