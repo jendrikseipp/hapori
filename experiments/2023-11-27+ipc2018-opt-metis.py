5049295fe623820c3407a0782d4fb3dfa569d671
Silvan Sievers <silvan.sievers@unibas.ch>
2024-01-02 11:59:33 +0100

    exps

--- experiments/2023-11-27+ipc2018-opt-metis.py
+++ experiments/2023-11-27+ipc2018-opt-metis.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import base_2023_11_27
 
 planner = base_2023_11_27.get_planner(__file__)
--- experiments/2023-11-27+ipc2018-symple2.py
+++ experiments/2023-11-27+ipc2018-symple2.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import base_2023_11_27
 
 planner = base_2023_11_27.get_planner_and_tracks(__file__)
