699d100eef2edc79a120a270ee88032cd2b8f0ad
Silvan Sievers <silvan.sievers@unibas.ch>
2024-01-24 11:39:20 +0100

    update component parser again

--- experiments/component_parser_runlim.py
+++ experiments/component_parser_runlim.py
@@ -23,7 +23,7 @@ def collect_plans_and_run_upv(content, props):
     run_dir = properties_file.parent
     plan_files = []
     for element in run_dir.iterdir():
-        if element.is_file() and element.name.startswith("sas_plan"):
+        if element.is_file() and (element.name == "sas_plan" or element.name.startswith("sas_plan.")):
             plan_files.append(element.name)
     plan_files = sorted(plan_files)
     if plan_files:
@@ -37,20 +37,9 @@ def collect_plans_and_run_upv(content, props):
                 cost = int(match.group(1))
                 props["upv_cost"] = cost
         for line in completed_process.stderr.splitlines():
-            if "Error: Plan failed to execute" in line:
+            if "Error: Plan failed to execute" in line or "Error: Goal not satisifed - Plan invalid" in line :
                 props["upv_invalid_plan"] = True
     props["plan_files"] = plan_files
-
-def coverage(content, props):
-    if "val_cost" in props and "upv_cost" in props and props["val_cost"] == props["upv_cost"]:
-        props["coverage"] = 1
-        props["cost"] = props["val_cost"]
-    else:
-        props.add_unexplained_error("mismatch between cost computed by VAL and UPV")
-        props["coverage"] = 0
-        props["cost"] = None
-    if props["coverage"]:
-        assert props["plan_files"]
 
 def custom_errors_stdout(content, props):
     lines = content.splitlines()
@@ -88,9 +77,26 @@ def unsupported(content, props):
         props["unsupported"] = False
 
 def set_outcome(content, props):
-    lines = content.splitlines()
+    # determine coverage
+    if "val_cost" in props:
+        props["coverage"] = 1
+        props["cost"] = props["val_cost"]
+    elif props["upv_cost"] is not None:
+        props["coverage"] = 1
+        props["cost"] = props["upv_cost"]
+    else:
+        props["coverage"] = 0
+        props["cost"] = None
     solved = props["coverage"]
-    unsupported = props["unsupported"]
+
+    # check for invalid plans
+    if props["val_invalid_plan"] or props["upv_invalid_plan"]:
+        props["invalid_plan"] = True
+    else:
+        props["invalid_plan"] = False
+    invalid_plan = props["invalid_plan"]
+
+    # check for out of time and/or memory status
     out_of_time = int(props["solver_status_num"] == 2)
     out_of_memory = int(props["solver_status_num"] == 3)
     if not out_of_time and not out_of_memory:
@@ -99,18 +105,17 @@ def set_outcome(content, props):
         if props.get("error") == "out_of_memory":
             out_of_memory = 1
     out_of_time_or_memory = 0
-    if props["val_invalid_plan"] ^ props["upv_invalid_plan"]:
-        props.add_unexplained_error("mismatch between invalid plan status determined by VAL and UPV")
-    invalid_plan = (props["val_invalid_plan"] or props["upv_invalid_plan"])
-    val_plan_too_long = props["val_plan_too_long"]
-    # print(solved, out_of_time, out_of_memory, unsupported, invalid_plan, val_plan_too_long)
+
+    # further possible outcome
+    unsupported = props["unsupported"]
+
+    # print(solved, out_of_time, out_of_memory, unsupported, invalid_plan)
     if (
         not solved
         and not out_of_time
         and not out_of_memory
         and not unsupported
-        and not invalid_plan
-        and not val_plan_too_long):
+        and not invalid_plan):
         if props["cpu_time"] > props["time_limit"]:
             out_of_time = 1
         elif props["cpu_time"]  > 1750 and props["used_memory"] > 7000:
@@ -137,7 +142,7 @@ def set_outcome(content, props):
         props["wall_time"] = None
 
     # print(solved, out_of_time, out_of_memory, out_of_time_or_memory, unsupported, invalid_plan)
-    if solved ^ out_of_time ^ out_of_memory ^ out_of_time_or_memory ^ unsupported ^ invalid_plan ^ val_plan_too_long:
+    if solved ^ out_of_time ^ out_of_memory ^ out_of_time_or_memory ^ unsupported ^ invalid_plan:
         if solved:
             props["error"] = "solved"
         elif out_of_time:
@@ -150,8 +155,6 @@ def set_outcome(content, props):
             props["error"] = "unsupported"
         elif invalid_plan:
             props["error"] = "invalid_plan"
-        elif val_plan_too_long:
-            props["error"] = "val_plan_too_long"
     else:
         props.add_unexplained_error(f"could not determine outcome")
         props["error"] = "unknown-outcome"
@@ -217,7 +220,6 @@ def get_parser():
     parser.add_function(val_plan_too_long)
     parser.add_function(val_invalid_plan)
     parser.add_function(collect_plans_and_run_upv)
-    parser.add_function(coverage)
     parser.add_function(custom_errors_stdout)
     parser.add_function(custom_errors_stderr, file="run.err")
     # also parse errors removed by fitler-stderr.py
@@ -231,7 +233,7 @@ def get_parser():
 if __name__ == "__main__":
     parser = get_parser()
     props = tools.Properties("properties")
-    run_dir = pathlib.Path(".").resolve()
+    run_dir = Path(".").resolve()
     parser.parse(run_dir, props)
     print(props)
     props.write()
