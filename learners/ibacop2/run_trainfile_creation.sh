829d8e9b8ac6192f37ed3ed374cf63238a239c97
Jendrik Seipp <jendrik.seipp@liu.se>
2024-03-07 19:37:25 +0100

    Update run_trainfile_creation.sh

--- learners/ibacop2/run_trainfile_creation.sh
+++ learners/ibacop2/run_trainfile_creation.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-
+# Satisficing
 # DATAFILE=../../experiments/data/training-data-collect-eval/properties-hardest-sat.json.xz
 # FTPATH=train_data/benchmark_features/ 
 # TRAINFILE=train_data/training/epm_train_sat_journal.csv
 # python read_trainexec.py $DATAFILE $FTPATH $TRAINFILE
 
+# Optimal
 DATAFILE=../../experiments/data/training-data-collect-eval/properties-hardest-opt.json.xz
 FTPATH=train_data/benchmark_features/ 
 TRAINFILE=train_data/training/epm_train_opt_journal.csv
 python read_trainexec.py $DATAFILE $FTPATH $TRAINFILE
-
-
-       \
-      
-
