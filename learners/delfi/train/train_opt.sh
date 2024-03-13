80a446c10bb1129769d9b90ffed07b72f565b335
Michael Katz <michael.katz1@ibm.com>
2024-03-13 14:21:46 +0000

    optimal training - files

--- learners/delfi/train/train_opt.sh
+++ learners/delfi/train/train_opt.sh
@@ -3,15 +3,18 @@
 
 SUFFIX="opt"
 FNAME="output_opt"
-RUNTIMEDATA='runtimes_opt.csv'
-SPLIT="split_opt.txt"
-SPLIT="split_opt_notest.txt"
+RUNTIMEDATA='full-opt-cpu_time.csv'
+RUNTIMEDATA='hardest-opt-cpu_time.csv'
+SPLIT="full-split_opt_notest.txt"
+SPLIT="full-split_opt.txt"
+SPLIT="hardest-split_opt_notest.txt"
+SPLIT="hardest-split_opt.txt"
 TIMEOUT="1800"
 IMAGES='images23'
 
 
-
 LABEL_TYPE=discrete  # choices: binary discrete, normalized, time
+#LABEL_TYPE=binary  # choices: binary discrete, normalized, time
 
 python image_based_network.py \
   --input data --output $FNAME \
